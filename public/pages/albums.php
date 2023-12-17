281a64e275b6e7608fc50ea0e730ccef9f6b64dd
barroscamila <camilagdbarros@gmail.com>
2023-12-17 18:18:31 -0300

    update file albums

--- public/pages/albums.php
+++ public/pages/albums.php
@@ -4,11 +4,14 @@
     <?php
         for ($i = 1; $i <= 10; $i++):
     ?>
-    <div class="col-3">
-        Album <?=$i;?> <!-- o mesmo que 'echo $i';--> 
+    <div class="col-3 album">
+        <a href="#">
+            <img src="albums/default.jpg" alt="<?=$i?>" class="img-album"> 
+            <!-- o mesmo que 'echo $i';--> 
+            <h4>Album <?=$i?></h4>
+        </a>
     </div>
     <?php
         endfor;
     ?>
-    
 </div>
